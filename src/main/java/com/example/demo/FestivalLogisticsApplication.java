fb88fead4db2c6d0c36d9474947805142d2e3a3d
SooMiguel <miguelangeluriartepacheco@gmail.com>
2026-04-16 20:10:53 -0500

    Commit inicial: Estructura base de Spring Boot con dependencias

---
+++ src/main/java/com/example/demo/FestivalLogisticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivalLogisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestivalLogisticsApplication.class, args);
+	}
+
+}
